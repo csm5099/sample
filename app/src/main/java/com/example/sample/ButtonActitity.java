1ef511436c2c5ef6a4c1b792a3e705af54e3408b
sungmin <csm5099@gmail.com>
2023-04-10 07:30:13 +0900

    버튼을 누르면 설치된 어플의 아이콘, 이름, 패키지명 리사이클러 뷰료 출력

---
+++ app/src/main/java/com/example/sample/ButtonActitity.java
@@ -0,0 +1,37 @@
+package com.example.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ButtonActitity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button_actitity);
+
+        final PackageManager pm = getPackageManager();
+        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        List<ApplicationInfo> list = new ArrayList<>();
+        for (ApplicationInfo packageInfo : packages){
+            Log.d("ApplicationINfo", "Installed package : " + packageInfo.packageName);
+            Log.d("ApplicationINfo", "Source dir : " + packageInfo.sourceDir);
+            Log.d("ApplicationINfo", "Launch Activity : " + pm.getLaunchIntentForPackage(packageInfo.packageName));
+
+            list.add(packageInfo);
+        }
+
+        ((RecyclerView)findViewById(R.id.adapter)).setAdapter(new AppAdapter(pm, list));
+        ((RecyclerView)findViewById(R.id.adapter)).setHasFixedSize((true));
+        ((RecyclerView)findViewById(R.id.adapter)).setLayoutManager((new LinearLayoutManager(this)));
+    }
+}
--- app/src/main/java/com/example/sample/MainActivity.java
+++ app/src/main/java/com/example/sample/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,20 +21,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final PackageManager pm = getPackageManager();
-        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
-        List<ApplicationInfo> list = new ArrayList<>();
-        for (ApplicationInfo packageInfo : packages){
-            Log.d("ApplicationINfo", "Installed package : " + packageInfo.packageName);
-            Log.d("ApplicationINfo", "Source dir : " + packageInfo.sourceDir);
-            Log.d("ApplicationINfo", "Launch Activity : " + pm.getLaunchIntentForPackage(packageInfo.packageName));
-
-            list.add(packageInfo);
-        }
-
-        ((RecyclerView)findViewById(R.id.adapter)).setAdapter(new AppAdapter(pm, list));
-        ((RecyclerView)findViewById(R.id.adapter)).setHasFixedSize((true));
-        ((RecyclerView)findViewById(R.id.adapter)).setLayoutManager((new LinearLayoutManager(this)));
+    }
+    
+    // 앱 추가 버튼을 눌렀을 때 설치된 앱 아이콘, 이름, 패키지 이름 목록을 보여줌
+    public void onAddButtonClicked(View v){
+        Intent showAppList = new Intent(getApplicationContext(), ButtonActitity.class);
+        startActivity(showAppList);
     }
 }
 
